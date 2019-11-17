46f09d624e754e3b39fa15970eb9ebe1439955da
Szanyi Krisztián <szkript@gmail.com>
2019-11-17 17:23:02 +0100

    not counting, check it later

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimpleThreadingDemo
 {
@@ -10,6 +11,23 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread thread1 = new Thread(threadStart);
+            Thread thread2 = new Thread(threadStart);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.ReadLine();
+        }
+
+        public static void Counting()
+        {
+            for (int i=1; i <= 10; i++)
+            {
+                Console.WriteLine($"Count : {0}, Thread : {1}", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
         }
     }
 }
